2fcf82ef37dac5f045ce16e50d55a293280a77d5
The Stoic Dev <stoicdev6@gmail.com>
2025-10-20 00:33:27 -0400

    rm: karma add: jest

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,18 @@
-import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dapp-dashboard');
+  it('should create the component', () => {
+    expect(fixture.componentInstance).toBeTruthy();
   });
 });
